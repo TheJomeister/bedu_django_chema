b378dc4419a5154cefdc1ebd1fce2ad439ef8bfe
Chema <amjo140942@gmail.com>
2021-11-18 20:53:41 -0600

    .

--- library/settings.py
+++ library/settings.py
@@ -130,7 +130,8 @@ STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'))
-DISABLE_COLLECTSTATIC
+
+DISABLE_COLLECTSTATIC = 0
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
